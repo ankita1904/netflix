44e9a64e57c71dc11303805c89db31b373ae0008
ankita1904 <ankitakalhapure@gmail.com>
2025-09-29 22:07:18 +0530

    Update analysis_directors.sql

--- DataAnalysis/analysis_directors.sql
+++ DataAnalysis/analysis_directors.sql
@@ -1,5 +1,5 @@
--- analysis_directors.sql
--- For each director count number of movies and tv shows (both) in separate columns
+-- Directors who have both Movies and TV Shows
+
 SELECT
     nd.director,
     COUNT(DISTINCT CASE WHEN ns.type = 'Movie' THEN ns.show_id END) AS num_movies,
@@ -7,5 +7,7 @@ SELECT
 FROM netflix_stg ns
 JOIN netflix_directors nd ON ns.show_id = nd.show_id
 GROUP BY nd.director
-HAVING num_movies > 0 AND num_tvshows > 0
-ORDER BY (num_movies + num_tvshows) DESC;
+HAVING COUNT(DISTINCT CASE WHEN ns.type = 'Movie' THEN ns.show_id END) > 0
+   AND COUNT(DISTINCT CASE WHEN ns.type = 'TV Show' THEN ns.show_id END) > 0
+ORDER BY (COUNT(DISTINCT CASE WHEN ns.type = 'Movie' THEN ns.show_id END) 
+          + COUNT(DISTINCT CASE WHEN ns.type = 'TV Show' THEN ns.show_id END)) DESC;
