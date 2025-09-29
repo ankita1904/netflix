c6b3b523e16fc01925882d1fe59c2eb2bc47ba80
ankita1904 <ankitakalhapure@gmail.com>
2025-09-29 22:09:22 +0530

    Update analysis_duration.sql

--- DataAnalysis/analysis_duration.sql
+++ DataAnalysis/analysis_duration.sql
@@ -2,9 +2,11 @@
 -- Average duration of movies in each genre
 SELECT
     ng.genre,
-    AVG(CAST(REPLACE(ns.duration, ' min', '') AS UNSIGNED)) AS avg_movie_duration
+    AVG(CAST(REPLACE(ns.duration, ' min', '') AS INT)) AS avg_movie_duration
 FROM netflix_stg ns
 JOIN netflix_genre ng ON ns.show_id = ng.show_id
-WHERE ns.type = 'Movie' AND ns.duration IS NOT NULL AND ns.duration != ''
+WHERE ns.type = 'Movie' 
+  AND ns.duration IS NOT NULL 
+  AND ns.duration != ''
 GROUP BY ng.genre
 ORDER BY avg_movie_duration DESC;
