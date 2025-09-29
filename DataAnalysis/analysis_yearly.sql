a47070c613fc6eaa0cff69b67316d14dd418056e
ankita1904 <ankitakalhapure@gmail.com>
2025-09-29 22:12:02 +0530

    Update analysis_yearly.sql

--- DataAnalysis/analysis_yearly.sql
+++ DataAnalysis/analysis_yearly.sql
@@ -1,14 +1,19 @@
 -- analysis_yearly.sql
--- For each year (as per date_added), which director has maximum number of movies released
+-- For each year as per date_added, which director has maximum number of movies released
 WITH director_counts AS (
-    SELECT YEAR(ns.date_added) AS year_added, nd.director, COUNT(*) AS cnt
+    SELECT year(ns.date_added) AS year_added, 
+           nd.director, 
+           COUNT(*) AS cnt
     FROM netflix_stg ns
-    JOIN netflix_directors nd ON ns.show_id = nd.show_id
-    WHERE ns.type = 'Movie' AND ns.date_added IS NOT NULL
-    GROUP BY YEAR(ns.date_added), nd.director
+    JOIN netflix_directors nd 
+      ON ns.show_id = nd.show_id
+    WHERE ns.type = 'Movie' 
+      AND ns.date_added IS NOT NULL
+    GROUP BY year(ns.date_added), nd.director
 ),
 ranked AS (
-    SELECT *, RANK() OVER (PARTITION BY year_added ORDER BY cnt DESC) AS rn
+    SELECT *,
+           RANK() OVER (PARTITION BY year_added ORDER BY cnt DESC) AS rn
     FROM director_counts
 )
 SELECT year_added, director, cnt
