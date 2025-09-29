4c25b3fcea200d3af21fee0b2e6a01ec6a82fa28
ankita1904 <ankitakalhapure@gmail.com>
2025-09-29 23:11:54 +0530

    Update analysis_comedy_horror.sql

--- DataAnalysis/analysis_comedy_horror.sql
+++ DataAnalysis/analysis_comedy_horror.sql
@@ -1,3 +1,5 @@
+%sql
+%run "/Workspace/Users/ankitakalhapure@gmail.com/Netflix/Extract"
 -- analysis_comedy_horror.sql
 -- Directors who have created both Comedy and Horror movies, with counts
 final_result_df = spark.sql("""
