7c06a76c4e0268a120470341ae6ba8f4cb531e2b
ankita1904 <ankitakalhapure@gmail.com>
2025-09-29 22:03:21 +0530

    Update analysis_country.sql

--- DataAnalysis/analysis_country.sql
+++ DataAnalysis/analysis_country.sql
@@ -1,10 +1,13 @@
 -- analysis_country.sql
 -- Country with highest number of comedy movies
-SELECT nc.country, COUNT(DISTINCT ng.show_id) AS num_comedy_movies
+SELECT 
+    TRIM(nc.country) AS country, 
+    COUNT(DISTINCT ng.show_id) AS num_comedy_movies
 FROM netflix_genre ng
 JOIN netflix_country nc ON ng.show_id = nc.show_id
 JOIN netflix_stg ns ON ng.show_id = ns.show_id
-WHERE ng.genre LIKE '%Comedy%' AND ns.type = 'Movie'
-GROUP BY nc.country
+WHERE TRIM(ng.genre) LIKE '%Comedy%' 
+  AND ns.type = 'Movie'
+GROUP BY TRIM(nc.country)
 ORDER BY num_comedy_movies DESC
 LIMIT 1;
